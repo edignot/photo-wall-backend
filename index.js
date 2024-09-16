ebb015d543f3f6557a20f406b5bfa8035b9b62a8
edita <edignot@gmail.com>
2024-09-15 19:28:49 -0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import cors from 'cors'
 import photoRoute from './routes/photoRoute.js'
 
 dotenv.config()
@@ -10,6 +11,8 @@ const { DATABASE_URL, PORT } = process.env
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use('/photos', photoRoute)
 
