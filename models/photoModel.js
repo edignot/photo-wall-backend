64451b9ff70a77f7ac889aa5583feb8197db2bb6
edita <edignot@gmail.com>
2024-09-15 20:32:59 -0600

    mongoose default version key disabled

--- models/photoModel.js
+++ models/photoModel.js
@@ -1,14 +1,19 @@
 import mongoose from 'mongoose'
 
-export const photoSchema = mongoose.Schema({
-    note: {
-        type: String,
-        required: false,
+export const photoSchema = mongoose.Schema(
+    {
+        note: {
+            type: String,
+            required: false,
+        },
+        photoUrl: {
+            type: String,
+            required: true,
+        },
     },
-    photoUrl: {
-        type: String,
-        required: true,
-    },
-})
+    {
+        versionKey: false,
+    }
+)
 
 export const Photo = mongoose.model('Photo', photoSchema)
