7399c5c0dda54450eecea4d2bb1f56a228199719
edita <edignot@gmail.com>
2024-09-15 18:10:58 -0600

    get and post photo routes created

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import { Photo } from './models/photoModel.js'
 
 dotenv.config()
 
 const { DATABASE_URL, PORT } = process.env
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', (request, response) => {
     return res.status(200).send('Home')
+})
+
+app.get('/photos', async (request, response) => {
+    try {
+        const photos = await Photo.find({})
+
+        return response.status(200).json(photos)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
+})
+
+app.post('/photos', async (request, response) => {
+    try {
+        if (!request.body.photoUrl) {
+            return response.status(400).send({
+                message: 'Photo is required',
+            })
+        }
+
+        const newPhoto = {
+            photoUrl: request.body.photoUrl,
+            note: request.body.note,
+        }
+
+        const photo = await Photo.create(newPhoto)
+
+        return response.status(201).send(photo)
+    } catch (error) {
+        console.log(error.message)
+        response.status(500).send({ message: error.message })
+    }
 })
 
 mongoose
