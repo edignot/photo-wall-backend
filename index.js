85b5a7d2f52e10695e62d577c57bca9ca89f7871
edita <edignot@gmail.com>
2024-09-15 19:38:22 -0600

    server cleanup

--- index.js
+++ index.js
@@ -11,23 +11,27 @@ const { DATABASE_URL, PORT } = process.env
 const app = express()
 
 app.use(express.json())
-
 app.use(cors())
-
 app.use('/photos', photoRoute)
 
 app.get('/', (request, response) => {
-    return res.status(200).send('Photos API')
+    const message = 'Welcome to the Photos API!'
+    const data = {
+        sourceCode: 'https://github.com/edignot/photo-wall-backend',
+    }
+
+    return response.status(200).json({ message, data })
 })
 
 mongoose
     .connect(DATABASE_URL)
     .then(() => {
         app.listen(PORT, () => {
-            console.log(`photo server is running on port : ${PORT}`)
+            console.log(`Photo server is running on port : ${PORT}`)
         })
-        console.log('database connected')
+        console.log('Database connected')
     })
     .catch((error) => {
-        console.log(error)
+        console.error('Error connecting to database:', error)
+        process.exit(1)
     })
